38b51781607bf5af714566d26827577e9f05f080
yaramvenki <venki.utk@gmail.com>
2021-07-09 01:09:38 +0530

    Game2048 - Gestures are added.

--- app/src/main/java/com/example/game2048/MainActivity.kt
+++ app/src/main/java/com/example/game2048/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.game2048
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.cell_item.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,9 +14,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val recyclerView: RecyclerView = recyclerView
+        val clMain: ConstraintLayout = clMain
 
-        var numbers = mutableListOf(
-            0,2,0,4,8,16,32,0,64,128,0,256,512,1024,0,2048
+        val numbers = mutableListOf(
+            0, 2, 0, 4, 8, 16, 32, 0, 64, 128, 0, 256, 512, 1024, 0, 2048
         )
 
         GridLayoutManager(
@@ -29,5 +30,39 @@ class MainActivity : AppCompatActivity() {
         }
 
         recyclerView.adapter = GameAdapter(numbers, this)
+
+
+
+        recyclerView.setOnTouchListener(object : SwipeTouchListener(this@MainActivity) {
+            override fun onSwipeLeft() {
+                super.onSwipeLeft()
+                Toast.makeText(
+                    this@MainActivity, "Swipe Left gesture detected",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+
+            override fun onSwipeRight() {
+                super.onSwipeRight()
+                Toast.makeText(
+                    this@MainActivity,
+                    "Swipe Right gesture detected",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun onSwipeUp() {
+                super.onSwipeUp()
+                Toast.makeText(this@MainActivity, "Swipe up gesture detected", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            override fun onSwipeDown() {
+                super.onSwipeDown()
+                Toast.makeText(this@MainActivity, "Swipe down gesture detected", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        })
     }
 }
